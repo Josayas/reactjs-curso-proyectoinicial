14c74f2c78d376a6a0f63f78244bb05f18c78b5a
Josias <Josias@Josiass-MacBook-Pro.local>
2020-07-02 21:27:56 -0400

    creacion de shop

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,31 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { auth } from "../../firebase/firebase.utils";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from '../../firebase/firebase.utils';
 
-import "./header.styles.scss";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
-  <div>
-    <div className="header">
-      <Link to="/" className="logo-container">
-        <Logo className="logo" style={{ width: "30px" }} />
-      </Link>
-    </div>
-    <div className="options">
-      <Link className="option" to="/shop">
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className="option" to="/contact">
-        CONTAT
+      <Link className='option' to='/contact'>
+        CONTACT
       </Link>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
+        <div className='option' onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="/signin">
+        <Link className='option' to='/signin'>
           SIGN IN
         </Link>
       )}
